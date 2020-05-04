de5d086953b35937dac6d1668817d42ffac89de7
aalsabag <ahmed.alsabag@gmail.com>
2020-05-04 02:32:27 -0500

    Finished parallel job execution and cleanup

--- kube_search.py
+++ kube_search.py
@@ -9,21 +9,22 @@ from kubernetes import client, config, utils
 from kubernetes.client.rest import ApiException
 
 import argparse
+import time
 
 #sequence_of_interest = Seq("NDVTSLISTTYPYTGPPPMSHGSSTKYTLETIKRTYDYSRTSVEKTSKVFNIPRRKFCNCLEDKDELVKP", generic_protein)
 
 config.load_kube_config()
 v1 = client.CoreV1Api()
 configuration = client.Configuration()
+api_instance = client.BatchV1Api(client.ApiClient(configuration))
+namespace = 'genome' # str | object name and auth scope, such as for teams and projects
 
-def create_job(job_name, sequence_of_interest):
-    api_instance = client.BatchV1Api(client.ApiClient(configuration))
-    namespace = 'genome' # str | object name and auth scope, such as for teams and projects
-    job_metadata = client.V1ObjectMeta(name = "genome-job", namespace = "genome")
+def create_job(job_name, sequence_of_interest,file_name, iteration):
+    job_metadata = client.V1ObjectMeta(name = job_name, namespace = namespace)
     job_container = client.V1Container(
-            name = job_name,
+            name = "genome-job",
             image="genome-job:latest",
-            args = ["--sequence", sequence_of_interest],
+            args = ["--sequence", sequence_of_interest, "--file", file_name, "--cut", "true", "--iteration", str(iteration)],
             image_pull_policy="Never")
     job_template = client.V1PodTemplateSpec(
             metadata=client.V1ObjectMeta(labels={"app": "sample"}),
@@ -37,20 +38,45 @@ def create_job(job_name, sequence_of_interest):
     body = client.V1Job(metadata = job_metadata, spec = job_spec) # V1Job | 
     field_manager = 'k8sGenomeSearch'
     api_response = api_instance.create_namespaced_job(namespace, body, field_manager=field_manager)
-    api_instance.read_namespaced_job_status
 
-def create_jobs(sequence):
+def create_jobs(sequence, file_name):
     #Create 10 jobs for now.
     #TODO need to split number of jobs based on number of sequences in the amino acid file
-    for x in range(0,10):
-        create_job(f"genome-job{x}",sequence)
+    for x in range(1,11):
+        create_job(job_name = f"genome-job{x}", sequence_of_interest = sequence, file_name = file_name, iteration = x)
+    
+    #wait for all jobs to finish
+    timeout = time.time() + 60*1   # 3 minutes from now
+    all_finished = False
+    break_from_loop = False
+    while True:
+        for x in range(1,11):
+            if (x == 10 and all_finished == True) or time.time() > timeout:
+                break_from_loop = True
+                break
+            else:
+                if api_instance.read_namespaced_job_status(f"genome-job{x}", namespace = namespace).status.active == None:
+                    all_finished = True
+                else:
+                    all_finished = False
+        if break_from_loop:
+            break
+    time.sleep(10)
+    for x in range(1,11):
+        pod_name = v1.list_namespaced_pod(namespace = namespace , label_selector=f'job-name=genome-job{x}').items[0].metadata.name
+        print(v1.read_namespaced_pod_log(namespace = namespace, name = pod_name))
+
+    #Delete all jobs
+    for x in range(1,11):
+        api_instance.delete_namespaced_job(namespace = namespace, name = f"genome-job{x}")
     
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--sequence',help="Sequence of proteins ie. NDVTSL", type = str)
+    parser.add_argument('-f', '--file', help="File name to be searched, it should exist in the image or a link to a downloadable file should be provided", type=str, default = "influenza_xsmall.faa")
     arguments = parser.parse_args()
 
-    create_jobs(arguments.sequence)
+    create_jobs(sequence = arguments.sequence, file_name = arguments.file)
 
     #regular_search(sequence_of_interest)
 
--- search.py
+++ search.py
@@ -3,28 +3,78 @@ from Bio.Alphabet import IUPAC
 from Bio import SeqIO
 from Bio.Alphabet import generic_dna, generic_protein
 import argparse
+import math
+import os
 
 sequence_of_interest = Seq("NDVTSLISTTYPYTGPPPMSHGSSTKYTLETIKRTYDYSRTSVEKTSKVFNIPRRKFCNCLEDKDELVKP", generic_protein)
 
-def regular_search(seq):
+def regular_search(seq, file_name):
     i = 0
     total_count = 0
-    for seq_record in SeqIO.parse("influenza_xsmall.faa", "fasta"):
+    for seq_record in SeqIO.parse(file_name, "fasta"):
         total_count += 1
         index = seq_record.seq.find(seq)
         if (index != -1):
             i += 1
-            print(f"Found Something. Item #{i}")
+            print(f"Found Something!")
             print(f"ID: {seq_record.id}")
-            print(f"Index in the sequence: {index}")
     print(total_count)
-            
+
+def split_file(file_name):
+    num_lines = sum(1 for line in open(file_name))
+    lines_per_file = math.ceil(num_lines / 10) #split into 10 files
+    smallfile = None
+    keep_going = False
+    file_number = 0
+    with open(file_name) as bigfile:
+        for lineno, line in enumerate(bigfile):
+            if lineno % lines_per_file == 0 or keep_going == True:
+                if line.startswith(">"): #make sure to split on a new entry
+                    if smallfile:
+                        smallfile.close()
+                    file_number += 1
+                    small_filename = 'small_file_{}_{}'.format(file_name,file_number)
+                    smallfile = open(small_filename, "w")
+                    keep_going = False
+                else:
+                    keep_going = True
+            smallfile.write(line)
+        if smallfile:
+            smallfile.close()
+
+def cleanup_split_files():
+    cwd = os.getcwd()
+    files_in_directory = os.listdir(cwd)
+    filtered_files = [file for file in files_in_directory if file.startswith("small_file")]
+    for file in filtered_files:
+        path_to_file = os.path.join(os.getcwd(), file)
+        os.remove(path_to_file)
+
+def download_file(file_name):
+    #TODO write logic to download a file
+    print("working on it")
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--sequence',help="Sequence of proteins ie. NDVTSL", type = str)
+    parser.add_argument('-f', '--file', help="Specify file for which to search. It should be in the container or be a downloadable link", type = str, default = "influenza_xsmall.faa", required = False)
+    parser.add_argument('-c', '--cut', help="Should we split the file into 10 files", type = bool, default = True)
+    parser.add_argument('-i','--iteration', help="If a cut was specified, specify the iteration of the file you want searched", type = int, required = False, default = 1)
     arguments = parser.parse_args()
-    regular_search(seq = arguments.sequence)
+    #split_file(arguments.file)
+    #cleanup_split_files()
+    if (arguments.file.startswith("http")):
+        #assume we need to download it
+        download_file(file_name = arguments.file)
+        file_name = arguments.file.split("/")[-1]
+
+    file_name = arguments.file
+    if arguments.cut:
+        split_file(arguments.file)
+        new_file_name = 'small_file_{}_{}'.format(file_name,arguments.iteration)
+
+    regular_search(seq = arguments.sequence, file_name = file_name)
 
 if __name__ == '__main__':
     main()
