a943fbd3b8436f2577f7e30baa7b9ed27208ccc0
aalsabag <ahmed.alsabag@gmail.com>
2020-05-06 00:36:15 -0500

    single job execution 46.54s

--- kube_search.py
+++ kube_search.py
@@ -19,7 +19,7 @@ configuration = client.Configuration()
 api_instance = client.BatchV1Api(client.ApiClient(configuration))
 namespace = 'genome' # str | object name and auth scope, such as for teams and projects
 
-def create_job(job_name, sequence_of_interest,file_name, iteration):
+def create_job(job_name, sequence_of_interest,file_name):
     job_metadata = client.V1ObjectMeta(name = job_name, namespace = namespace)
     job_volume = client.V1Volume(
             host_path = client.V1HostPathVolumeSource(path = os.getcwd().replace("\\","/").replace("C:", "/c")),
@@ -28,17 +28,19 @@ def create_job(job_name, sequence_of_interest,file_name, iteration):
     job_volume_mount = client.V1VolumeMount(
             mount_path = "/app",
             name = "genome-volume")
-    
-    job_container = client.V1Container(
-            name = "genome-job",
-            image="genome-job:latest",
-            args = ["--sequence", sequence_of_interest, "--file", file_name],
-            volume_mounts=[job_volume_mount],
-            image_pull_policy="Never")
+    job_container_array = []
+    for x in range(1,11):
+        job_container = client.V1Container(
+                name = f"genome-job{x}",
+                image="genome-job:latest",
+                args = ["--sequence", sequence_of_interest, "--file", f"{file_name}_{x}"],
+                volume_mounts=[job_volume_mount],
+                image_pull_policy="Never")
+        job_container_array.append(job_container)
     job_template = client.V1PodTemplateSpec(
             metadata=client.V1ObjectMeta(labels={"app": "sample"}),
             spec=client.V1PodSpec(restart_policy="Never", 
-                                  containers=[job_container],
+                                  containers=job_container_array,
                                   volumes = [job_volume]))
     job_spec = client.V1JobSpec(
             template=job_template,
@@ -52,34 +54,25 @@ def create_job(job_name, sequence_of_interest,file_name, iteration):
 def create_jobs(sequence, file_name):
     #Create 10 jobs for now.
     #TODO need to split number of jobs based on number of sequences in the amino acid file
-    for x in range(1,11):
-        create_job(job_name = f"genome-job{x}", sequence_of_interest = sequence, file_name = f"{file_name}_{x}", iteration = x)
+    
+    create_job(job_name = "genome-job", sequence_of_interest = sequence, file_name = file_name)
     
     #wait for all jobs to finish
     timeout = time.time() + 60*1   # 3 minutes from now
     all_finished = False
     break_from_loop = False
     while True:
-        for x in range(1,11):
-            if (x == 10 and all_finished == True) or time.time() > timeout:
-                break_from_loop = True
-                break
-            else:
-                status = api_instance.read_namespaced_job_status(f"genome-job{x}", namespace = namespace).status
-                if status.active == None and (status.succeeded == 1):
-                    all_finished = True
-                else:
-                    all_finished = False
-        if break_from_loop:
+        status = api_instance.read_namespaced_job_status("genome-job", namespace = namespace).status
+        if status.active == None and (status.succeeded == 1):
             break
-    #time.sleep()
+                
+    
+    pod_name = v1.list_namespaced_pod(namespace = namespace , label_selector=f'job-name=genome-job').items[0].metadata.name
     for x in range(1,11):
-        pod_name = v1.list_namespaced_pod(namespace = namespace , label_selector=f'job-name=genome-job{x}').items[0].metadata.name
-        print(v1.read_namespaced_pod_log(namespace = namespace, name = pod_name))
+        print(v1.read_namespaced_pod_log(namespace = namespace, name = pod_name,container = f"genome-job{x}" ))
 
-    #Delete all jobs
-    for x in range(1,11):
-        api_instance.delete_namespaced_job(namespace = namespace, name = f"genome-job{x}")
+    
+    api_instance.delete_namespaced_job(namespace = namespace, name = "genome-job")
     
 def main():
     parser = argparse.ArgumentParser()
@@ -88,7 +81,8 @@ def main():
     arguments = parser.parse_args()
 
     start_time = time.time() #Timing execution
-    create_jobs(sequence = arguments.sequence, file_name = arguments.file)
+    #################################create_jobs(sequence = arguments.sequence, file_name = arguments.file)
+    create_jobs(file_name=arguments.file,sequence=arguments.sequence)
     print("--- %s seconds ---" % (time.time() - start_time))
     #regular_search(sequence_of_interest)
 
